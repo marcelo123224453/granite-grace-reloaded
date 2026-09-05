df4deb7009912abe8d5eaf55c8a552ececbea2c6
marcelo123224453 <marcelserwin09@gmail.com>
2026-09-05 20:17:27 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ import nagrobek2 from "@/assets/nagrobek-2.webp";
 import nagrobek3 from "@/assets/nagrobek-3.webp";
 import nagrobek4 from "@/assets/nagrobek-4.webp";
 import nagrobek5 from "@/assets/nagrobek-5.webp";
-import warsztat from "@/assets/nagrobek-warsztat.webp";
+import warsztat from "@/assets/nagrobek-detal.webp";
 import detal from "@/assets/ChatGPT Image 5 wrz 2026, 19_54_53.png";
 
 export const Route = createFileRoute("/")({
